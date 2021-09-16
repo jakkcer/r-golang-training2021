af6bc4841af539351e9252f887f3329481e0eb87
jakkcer <jakkcer.fivework@gmail.com>
2021-09-16 12:00:39 +0900

    練習問題1.9

---
+++ ch01/ex09/fetch_test.go
@@ -0,0 +1,49 @@
+// Fetchのテスト
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+type Response struct {
+	path, contentType, body string
+}
+
+func TestFetch(t *testing.T) {
+	response := &Response{
+		path:        "/test",
+		contentType: "application/json",
+		body:        "test body",
+	}
+	handler := func(w http.ResponseWriter, r *http.Request) {
+		if g, w := r.URL.Path, response.path; g != w {
+			t.Errorf("request got path %s, want %s", g, w)
+		}
+		w.Header().Set("Content-Type", response.contentType)
+		io.WriteString(w, response.body)
+	}
+
+	// モックサーバ
+	server := httptest.NewServer(http.HandlerFunc(handler))
+	defer server.Close()
+
+	testInput := server.URL + "/test"
+	testWant := "HTTP status: 200 OK\n\ntest body"
+
+	descr := fmt.Sprintf("fetch(%q)", testInput)
+
+	out = new(bytes.Buffer)
+	if err := fetch(testInput); err != nil {
+		t.Errorf("%s failed: %v", descr, err)
+	}
+
+	got := out.(*bytes.Buffer).String()
+	if got != testWant {
+		t.Errorf("%s = %q, want %q", descr, got, testWant)
+	}
+}
