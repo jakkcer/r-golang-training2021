b4d3e5e0c5f72f8fb634b4a21b3f24d320a541b1
jakkcer <jakkcer.fivework@gmail.com>
2021-11-06 22:47:14 +0900

    練習問題5.12

---
+++ ch05/ex12/outline.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+
+	"golang.org/x/net/html"
+)
+
+func main() {
+	for _, url := range os.Args[1:] {
+		outline(url)
+	}
+}
+
+func outline(url string) error {
+	resp, err := http.Get(url)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	doc, err := html.Parse(resp.Body)
+	if err != nil {
+		return err
+	}
+
+	var depth int
+
+	startElement := func(n *html.Node) {
+		if n.Type == html.ElementNode {
+			fmt.Printf("%*s<%s>\n", depth*2, "", n.Data)
+			depth++
+		}
+	}
+
+	endElement := func(n *html.Node) {
+		if n.Type == html.ElementNode {
+			depth--
+			fmt.Printf("%*s</%s>\n", depth*2, "", n.Data)
+		}
+	}
+
+	forEachNode(doc, startElement, endElement)
+
+	return nil
+}
+
+// forEachNodeはnから始まるツリー内の個々のノードxに対して
+// 関数pre(x)とpost(x)を呼び出します．その二つの関数はオプションです．
+// preは子ノードを訪れる前に呼び出され(前順:preorder)，
+// postは子ノードを訪れた後に呼び出されます(後順:postorder)．
+func forEachNode(n *html.Node, pre, post func(n *html.Node)) {
+	if pre != nil {
+		pre(n)
+	}
+
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		forEachNode(c, pre, post)
+	}
+
+	if post != nil {
+		post(n)
+	}
+}
