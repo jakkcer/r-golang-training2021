884ffcdd30a422f6565dd5057facc725a25b8108
jakkcer <jakkcer.fivework@gmail.com>
2021-11-06 20:51:49 +0900

    練習問題5.10

---
+++ ch05/ex10/toposort.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"fmt"
+)
+
+var prereqs = map[string]map[string]string{
+	"algorithms": {"data structures": ""},
+	"calculus":   {"linear algebra": ""},
+	"compilers": {
+		"data structures":       "",
+		"formal languages":      "",
+		"computer organization": "",
+	},
+	"data structures":       {"discrete math": ""},
+	"databases":             {"data structures": ""},
+	"discrete math":         {"intro to programming": ""},
+	"formal languages":      {"discrete math": ""},
+	"networks":              {"operating systems": ""},
+	"operating systems":     {"data structures": "", "computer organization": ""},
+	"programming languages": {"data structures": "", "computer organization": ""},
+}
+
+func main() {
+	for i, course := range topoSort(prereqs) {
+		fmt.Printf("%d:\t%s\n", i+1, course)
+	}
+}
+
+func topoSort(m map[string]map[string]string) []string {
+	var order []string
+	seen := make(map[string]bool)
+	var visitAll func(items map[string]string)
+
+	visitAll = func(items map[string]string) {
+		for item := range items {
+			if !seen[item] {
+				seen[item] = true
+				visitAll(m[item])
+				order = append(order, item)
+			}
+		}
+	}
+
+	keys := make(map[string]string, len(m))
+	for key := range m {
+		keys[key] = ""
+	}
+
+	visitAll(keys)
+	return order
+}
